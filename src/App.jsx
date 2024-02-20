6bcb07d9b1499528a366b51afafd26112633b5d2
Ayush <kumarayush9694@gmail.com>
2024-02-20 16:11:09 +0530

    added postalcode api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Debounce from "./components/Debounce"
 
 function App() {
  
   return (
-    <>
-      
-    </>
+    <div className="w-screen h-screen">
+      <Debounce/>
+    </div>
   )
 }
 
--- src/components/Debounce.jsx
+++ src/components/Debounce.jsx
@@ -1,7 +1,28 @@
-export default function Debounce(){
-    return(
-        <>
-            
-        </>
-    )
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Debounce() {
+  const [pincode, setPincode] = useState("");
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      console.log({ pincode });
+      const { data } = await axios.get(
+        `https://api.postalpincode.in/pincode/${pincode}`
+      );
+      console.log({ data });
+    }, 1000);
+    return () => clearTimeout(timeoutId);
+  }, [pincode]);
+  return (
+    <div className="w-screen  flex flex-col items-center justify-center gap-2 p-10 ">
+      <span className="text-xl font-semibold ">Search your pincode</span>
+      <input
+        placeholder="Search pincode"
+        className="border border-black rounded-md "
+        value={pincode}
+        onChange={(e) => setPincode(e.target.value)}
+      />
+    </div>
+  );
 }
